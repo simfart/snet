0da7075730a164570982ba70a6f2af5ea88a6fe9
Art <simf.i@ya.ru>
2024-12-11 23:23:10 +0300

    fix bugs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider, RoutesProvider } from './providers';
-
+import { ScrollToTop } from 'shared/hooks/scrollToTop';
 import './styles/global.scss';
 
 export const App: FC = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <QueryClientProvider>
         <RoutesProvider />
       </QueryClientProvider>
