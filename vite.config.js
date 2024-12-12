b2ebc0f3aef3c3d7c50d5a53a25e896a7f03a396
Art <simf.i@ya.ru>
2024-12-12 23:06:59 +0300

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-    plugins: [react()],
-    base: '/snet/',
-    resolve: {
-        alias: {
-            app: path.resolve(__dirname, './src/app'),
-            pages: path.resolve(__dirname, './src/pages'),
-            shared: path.resolve(__dirname, './src/shared'),
-            features: path.resolve(__dirname, './src/features'),
-            entities: path.resolve(__dirname, './src/entities'),
-            widgets: path.resolve(__dirname, './src/widgets'),
-        },
+  plugins: [react()],
+  base: '/snet/',
+  resolve: {
+    alias: {
+      app: path.resolve(__dirname, './src/app'),
+      pages: path.resolve(__dirname, './src/pages'),
+      shared: path.resolve(__dirname, './src/shared'),
+      features: path.resolve(__dirname, './src/features'),
+      entities: path.resolve(__dirname, './src/entities'),
+      widgets: path.resolve(__dirname, './src/widgets'),
     },
+  },
 });
